15d371b1d34892e50899d6e0cfb3d47bc1683fa9
Saurabh Rajesh Kshirsagar <kshirsagarsrk@gmail.com>
2021-03-25 19:41:49 +0530

    MongoDB Configuration Information

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import urllib.parse
 class DB_connection:
 
     def __init__(self):
-        self.user_name = urllib.parse.quote_plus('Saurabh_Ksagar')
-        self.password = urllib.parse.quote_plus('Saurabh@123srk')
-        self.database_name = "Stock_Data"
-        self.collection_Name = "Companies"
+        self.user_name = urllib.parse.quote_plus('UserName_for_your_mongo')
+        self.password = urllib.parse.quote_plus('Password_for_your_mongo')
+        self.database_name = "Your_Database_name"
+        self.collection_Name = "Your_Collection_name"
